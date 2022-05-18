dfa9d8d3d9f6293211c2dbad9c915e585e4a6516
Roman <roman.motrich@yandex.ru>
2022-05-18 14:48:26 +0300

    defaults

--- project/js/main.js
+++ project/js/main.js
@@ -3,19 +3,22 @@ const products = [
     {id: 2, title: 'Mouse', price: 20,img:'../src/img/mouse.jpg'},
     {id: 3, title: 'Keyboard', price: 200,img:'../src/img/keyboard.jpg'},
     {id: 4, title: 'Gamepad', price: 50,img:'../src/img/gamepad.jpg'},
+    {id: 5, title: 'Без рисунка', price: 50},
+    {id: 6, title: 'Без рисунка и цены',},
+    {id:7},
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (product) => {
+const renderProduct = (title='Товар',price=0,img='../src/img/nothing.jpg') => {
     return `<div class="product-item">
-                <img src="${product.img}" width="150" height="150">
-                <h3>${product.title}</h3>
-                <p>${product.price}</p>
+                <img src="${img}" width="150" height="150">
+                <h3>${title}</h3>
+                <p>${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
+    const productsList = list.map(item => renderProduct(item.title,item.price,item.img));
     console.log(productsList);
     document.querySelector('.products').innerHTML = productsList;
 };
