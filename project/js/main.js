eff3832e6d81abcd93e770bfe0a9c6064da8fb83
Roman <roman.motrich@yandex.ru>
2022-05-25 08:37:57 +0300

    Кнопка жмяк

--- project/js/main.js
+++ project/js/main.js
@@ -96,6 +96,8 @@ class GoodsItem {
         this._getSum();//суммарная стоимость одной позиции
     }
 
+    
+
     _getSum() {
         this.summa = this.product.price * this.quantity
     }
@@ -112,40 +114,18 @@ class GoodsItem {
     }
 }
 
-
+class Card{
+    constructor(){    
+        this._init();
+    }
+    
+    _init(){
+        document.querySelector('.btn-cart').addEventListener('click',()=>{
+            console.log('basket pressed')
+        });
+    }  
+}
 
 //Покажем
 let list = new ProductList();
-
-
-
-
-
-
-
-// const products = [
-//     {id: 1, title: 'Notebook', price: 2000,img:'../src/img/noutbuk.jpg'},
-//     {id: 2, title: 'Mouse', price: 20,img:'../src/img/mouse.jpg'},
-//     {id: 3, title: 'Keyboard', price: 200,img:'../src/img/keyboard.jpg'},
-//     {id: 4, title: 'Gamepad', price: 50,img:'../src/img/gamepad.jpg'},
-//     {id: 5, title: 'Без рисунка', price: 50},
-//     {id: 6, title: 'Без рисунка и цены',},
-//     {id:7},
-// ];
-// //Функция для формирования верстки каждого товара
-// //Добавить в выводе изображение
-// const renderProduct = (title='Товар',price=0,img='../src/img/nothing.jpg') => {
-//     return `<div class="product-item">
-//                 <img src="${img}" width="150" height="150">
-//                 <h3>${title}</h3>
-//                 <p>${price}</p>
-//                 <button class="buy-btn">Купить</button>
-//             </div>`
-// };
-// const renderPage = list => {
-//     const productsList = list.map(item => renderProduct(item.title,item.price,item.img));
-//     console.log(productsList);
-//     document.querySelector('.products').innerHTML = productsList;
-// };
-
-// renderPage(products);
+let card = new Card();
